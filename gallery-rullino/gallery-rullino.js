546819629e284220a6447f9838e575a5cd4852d5
Flavia <favilla.flavia@gmail.com>
2019-03-20 14:38:48 +0100

    fix images more than 3

--- gallery-rullino/gallery-rullino.js
+++ gallery-rullino/gallery-rullino.js
@@ -8,7 +8,7 @@ console.log(windowWidth);
     $('.carousel-showmanymoveone .item').each(function(){
       var itemToClone = $(this);
   
-      for (var i=1;i<3;i++) {
+      for (var i=1;i<5;i++) {
         itemToClone = itemToClone.next();
   
         // wrap around if at end of item collection
