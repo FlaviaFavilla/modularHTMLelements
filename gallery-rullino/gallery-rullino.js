80eb516551bd80a8b0af9a8e68c100a320abd130
Flavia <favilla.flavia@gmail.com>
2019-03-28 09:38:26 +0100

    roll gallery

--- gallery-rullino/gallery-rullino.js
+++ gallery-rullino/gallery-rullino.js
@@ -3,17 +3,19 @@ $(document).ready(function(){
 // -----------  SELEZIONE TUTTI gli slider, funzioni al clicl sul singolo  -----------
   $('.component-galleryRoll .component-galleryRoll-row-img.transition').each(function(index) {
 
-    var carousel = $(this).find(".carousel.carousel-showmanymoveone");
-    var rowContainer = $(this).find(".carousel.carousel-showmanymoveone.carousel-fullWidth");
-    var carouselOpen = $(this).find(".component-galleryRoll-row-arrow");
-    var carouselControls =  $(this).find(".carousel-controls");
-    var carouselControlsLink =  $(this).find(".carousel-controls > a");
-    var carouselIndicators =  $(this).find(".carousel-indicators");
+    // var carouselToSlide =  $(this);
+    var self = $(this);
+
+    var carousel = self.find(".carousel.carousel-showmanymoveone");
+    var rowContainer = self.find(".carousel.carousel-showmanymoveone.carousel-fullWidth");
+    var carouselOpen = self.find(".component-galleryRoll-row-arrow");
+    var carouselControls =  self.find(".carousel-controls");
+    var carouselControlsLink =  self.find(".carousel-controls > a");
+    var carouselIndicators =  self.find(".carousel-indicators");
     // var carouselIndicatorLink =  $(this).find(".carousel-indicators li");
-    var carouselClose =  $(this).find(".carousel-close");
-    var carouselItems =  $(this).find(".carousel-inner > .item");
+    var carouselClose =  self.find(".carousel-close");
+    var carouselItems =  self.find(".carousel-inner > .item");
     // var carouselItemList =  $(this).find(".carousel-inner > .item > .item-list");
-    var carouselToSlide =  $(this);
 
     // var carouselItemsLeft =  $(this).find(".carousel-inner-toLeft > .item");
     // var carouselItemsRight =  $(this).find(".carousel-inner-toRight > .item");
@@ -21,8 +23,8 @@ $(document).ready(function(){
 // carouselItems = carouselItemsLeft > 0 ? carouselItemsLeft : carouselItemsRight.get().reverse();
 // console.log(carouselItemsLeft, carouselItemsRight );
 
-    var parent = $(this).parent(); 
-    var parents = $(this).parents().hasClass('toLeft'); 
+    var parent = self.parent(); 
+    var parents = self.parents().hasClass('toLeft'); 
 
 // console.log(carouselItems.length);
     // -----------  genera la dimesione del container per le slides  -----------
@@ -71,7 +73,7 @@ $(document).ready(function(){
       var limiTransition = $(".component-galleryRoll-row.multImg.toLeft .slide.carousel-fullWidth").offset();
       var calc = Math.abs(limitContainer.left - limiTransition.left);
 
-      parents ? carouselToSlide.css('transform', 'translate( -'+ calc +'px )') : carouselToSlide.css('transform', 'translate( '+ calc +'px )');
+      parents ? self.css('transform', 'translate( -'+ calc +'px )') : self.css('transform', 'translate( '+ calc +'px )');
 
       // --- a fine animazione modifica visibiltà elementi  -----
       parent.one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend",
@@ -86,7 +88,7 @@ $(document).ready(function(){
     carouselClose.on("click", function(e){
       e.stopPropagation();
 
-      parents ? carouselToSlide.css('transform', '') : carouselToSlide.css('transform', '');
+      parents ? self.css('transform', '') : self.css('transform', '');
 
       // --- a fine animazione modifica visibiltà elementi  -----
       parent.one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend",
@@ -101,24 +103,37 @@ $(document).ready(function(){
       console.log( $(this) );
     })
 
+    // var self = $(this);
 
-    // var next = carouselControls.find('[data-slide="next"]');
-    // var itemActiveLast3 =  $(this).find(".item-last3.active");
-    // var itemActiveLast3active =  $(this).find(".item-last3");
+    var next = carouselControls.find('.right.carousel-control');
+    var prev = carouselControls.find('.left.carousel-control');
 
-    // next.on('click', function(){
-    //   var limitContainerWidth = $(".limit-container").width();
-    //   var itemActive3 = $(".carousel-inner > .item.item-last3.active");
-    //   var itemActiveLast4 = $(".carousel-inner > .item.item-last4.active").length;
-    //   var itemActive4 = $(".carousel-inner > .item.item-last4.active");
-    //   console.log('3: ' + itemActiveLast3.length);
-    //   console.log('active: ' + itemActiveLast3active.length);
-    //   if(limitContainerWidth > 1680){
-    //     // if(itemActiveLast4 == 1)  console.log('4 ', itemActive4);
-    //     if(itemActiveLast3 == 1)  console.log('3 ', itemActive3);
-    //     // next.css('pointer-events','none');
-    //   }
-    // })
+    var itemActiveLast3active =  self.find(".item-last3");
+
+    next.on('click', function(){
+      var itemActiveLast3 =  self.find(".item-last4.active");
+
+      var limitContainerWidth = $(".limit-container").width();
+      var itemActive3 = $(".carousel-inner > .item.item-last3.active");
+      var itemActiveLast4 = $(".carousel-inner > .item.item-last4.active").length;
+      var itemActive4 = $(".carousel-inner > .item.item-last4.active");
+      console.log('3: ' + itemActiveLast3.length);
+      console.log('active: ' + itemActiveLast3active.length);
+      console.log({limitContainerWidth,itemActiveLast3, self,next });
+
+      
+      if(limitContainerWidth > 1680){
+        // if(itemActiveLast4 == 1)  console.log('4 ', itemActive4);
+        if(itemActiveLast3 && itemActiveLast3.length) {
+          next.hide();
+        }
+
+      }
+
+    })
+    prev.on('click', function(){
+      next.show()
+    })
 
   });
 });
